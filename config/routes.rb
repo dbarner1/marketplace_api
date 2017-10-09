f632405f769b4a99eeea01d4a6b1c4d295fb2cfa
Dave Barner <dbarner@thelevelup.com>
2017-10-09 14:21:34 -0400

    Add Product Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :products
   resources :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   Rails.application.routes.draw do
+  resources :products
     resources :users do
       resources :first_name
     end
