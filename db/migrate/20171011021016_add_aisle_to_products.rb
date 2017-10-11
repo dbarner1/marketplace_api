d63c09d4f6a848f9369259da740cf88b57786f04
Dave Barner <dbarner@gmail.com>
2017-10-10 22:25:40 -0400

    Add Aisle to Products Fix

--- db/migrate/20171011021016_add_aisle_to_products.rb
+++ db/migrate/20171011021016_add_aisle_to_products.rb
@@ -1,5 +1,5 @@
 class AddAisleToProducts < ActiveRecord::Migration[5.0]
   def change
-  add_column :products, :aisle, :text
+  add_column :products, :aisle, :string
   end
 end
--- db/migrate/20171011021652_edit_aisle_to_string.rb
+++
@@ -1,5 +0,0 @@
-class EditAisleToString < ActiveRecord::Migration[5.0]
-  def change
-    change_column :products, :aisle, :string
-  end
-end
